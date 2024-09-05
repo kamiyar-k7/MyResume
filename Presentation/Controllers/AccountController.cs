0c931a85bd4303175e35f695fc3bb59b2ad9b56d
Kamyar Khedri <kami.kh0038@gmail.com>
2024-09-05 13:45:26 +0330

    admin login fixed

--- Presentation/Controllers/AccountController.cs
+++ Presentation/Controllers/AccountController.cs
@@ -5,60 +5,59 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace Presentation.Controllers
+namespace Presentation.Controllers;
+
+public class AccountController : Controller
 {
-    public class AccountController : Controller
+
+    #region Ctor
+    private readonly IUserInformationService _userInformationService;
+    public AccountController(IUserInformationService userInformationService)
+    {
+        _userInformationService = userInformationService;
+    }
+    #endregion
+
+    #region Login
+    [HttpGet]
+    public IActionResult LogIn()
     {
 
-        #region Ctor
-        private readonly IUserInformationService _userInformationService;
-        public AccountController(IUserInformationService userInformationService)
-        {
-            _userInformationService = userInformationService;
-        }
-        #endregion
-
-        #region Login
-        [HttpGet]
-        public IActionResult LogIn()
-        {
-
-            return View();
-        }
-        [HttpPost, ValidateAntiForgeryToken]
-        public async Task<IActionResult> LogIn(AdminLoginDto model)
-        {
-            if (ModelState.IsValid)
-            {
-                var admin = await _userInformationService.LogIn(model);
-                if (admin)
-                {
-                    var claims = new List<Claim>
-                      {
-                         new (ClaimTypes.NameIdentifier, model.Id.ToString()),
-                         new (ClaimTypes.Name , model.Id.ToString()),
-
-
-                      };
-
-                    var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    var principal = new ClaimsPrincipal(claimIdentity);
-
-                    var authProps = new AuthenticationProperties();
-                    authProps.IsPersistent = model.RememberMe;
-
-                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, authProps);
-                    
-                    return RedirectToAction("Index", "Home");
-                   
-
-                }
-              
-            }
-            TempData["ErrorMessage"] = "Failed to log in. Please check your inputs.";
-            return View();
-        }
-        #endregion
-
+        return View();
     }
+    [HttpPost, ValidateAntiForgeryToken]
+    public async Task<IActionResult> LogIn(AdminLoginDto model)
+    {
+        if (ModelState.IsValid)
+        {
+            var admin = await _userInformationService.LogIn(model);
+            if (admin)
+            {
+                var claims = new List<Claim>
+                  {
+                     new (ClaimTypes.NameIdentifier, model.Id.ToString()),
+                     new (ClaimTypes.Name , model.Id.ToString()),
+
+
+                  };
+
+                var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(claimIdentity);
+
+                var authProps = new AuthenticationProperties();
+                authProps.IsPersistent = model.RememberMe;
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, authProps);
+                
+                return RedirectToAction("Index", "Home" , new {area = "Admin"});
+               
+
+            }
+          
+        }
+        TempData["ErrorMessage"] = "Failed to log in. Please check your inputs.";
+        return View();
+    }
+    #endregion
+
 }
